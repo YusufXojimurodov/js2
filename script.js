ecca417c88d03557a6ad0fb3274cf41037826db0
YusufXojimurodov <xojimurodovyusufjon2@gmail.com>
2025-04-03 20:49:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let string = prompt('String xabarni kiriting')
+
+let number = +prompt('Number turidagi xabarni kiriting')
+
+let booling = prompt('Booling turidagi xabarni kiriting')
+
+let areyousure = confirm('Kiritgan malumotlarngizga ishonchingiz komilmi ?')
+
+let javob = alert(
+    'Xabar turi: ' + typeof string + '\n' +
+    'Xabar turi: ' + typeof number + '\n' +
+    'Xabar turi boolean: ' + booling
+);
